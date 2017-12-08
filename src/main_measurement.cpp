0b63f6483cf019ff231b0fd5a310a5bbba2bf810
Hendrik Schmidt <hfrschmidt@gmail.com>
2017-12-08 12:06:43 +0100

    Reorganized build process to include targets for testing, demonstration (default target) and measurements of performance.

--- src/main_measurement.cpp
+++ src/main_measurement.cpp
@@ -1,9 +1,10 @@
 #include "../include/crypto_diffieHellman.h"
 
+
 int main(int argc, char** argv) {
     
     Crypto::CDiffieHellman dh = Crypto::CDiffieHellman();
-    dh.printExchangeSteps();
+    dh.profileExchange();
     
     return 0;
 }
